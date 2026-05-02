90846ac8da749d75ab29a6595f44fdd0489cd0b5
llwizardoj <112179748+ashiharayusuke@users.noreply.github.com>
2026-05-02 17:20:09 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -286,7 +286,7 @@ function startQuiz(allMode = false) {
   loadQuiz();
 }
 function loadQuiz() {
-　questionNumberEl.textContent = `第${currentQuiz + 1}問 / ${quizLimit+1}問`;
+　questionNumberEl.textContent = `第${currentQuiz + 1}問 / ${quizLimit}問`;
   nextBtn.disabled = true;
   answerResultEl.textContent = "";
   explanationEl.textContent = "";
